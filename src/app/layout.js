9ecbdf296f1da8c52ea82b6763bf5efea4a644ab
shahmeer <theshahmeerfsd@gmail.com>
2023-10-13 16:20:45 +0500

    Design Structure, Styles, Layouts, Containers

---
+++ src/app/layout.js
@@ -0,0 +1,27 @@
+import Footer from '../components/footer/Footer'
+import Navbar from '../components/navbar/Navbar'
+import { ThemeContextProvider } from '../context/ThemeContext'
+import ThemeProvider from '../providers/ThemeProvider'
+import './globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Blog App',
+  description: 'The best blog app!',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+            <div className='container'>
+              <div className='wrapper'>
+                {children}
+              </div>
+            </div>
+      </body>
+    </html>
+  )
+}
