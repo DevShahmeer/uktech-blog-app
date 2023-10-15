f179a7097efed5e29a99664f3689d00d1428414e
shahmeer <theshahmeerfsd@gmail.com>
2023-10-15 14:35:27 +0500

    Added OAuthJS authentication

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import Footer from '../components/footer/Footer'
 import Navbar from '../components/navbar/Navbar'
 import { ThemeContextProvider } from '../context/ThemeContext'
+import AuthProvider from '../providers/AuthProvider'
 import ThemeProvider from '../providers/ThemeProvider'
 import './globals.css'
 import { Inter } from 'next/font/google'
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <AuthProvider>
           <ThemeContextProvider>
             <ThemeProvider>
               <div className="container">
@@ -27,6 +29,7 @@ export default function RootLayout({ children }) {
               </div>
             </ThemeProvider>
           </ThemeContextProvider>
+        </AuthProvider>
       </body>
     </html>
   )
