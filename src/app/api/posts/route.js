f25013876d0b7d8dd99e0bd8ea87d2d5701a591f
shahmeer <theshahmeerfsd@gmail.com>
2023-10-15 20:20:16 +0500

    Completed schema setup and categories API

---
+++ src/app/api/posts/route.js
@@ -0,0 +1,27 @@
+import prisma from "@/src/utils/connect";
+import { NextResponse } from "next/server";
+
+export const POST = async () => {
+
+    const {searchParams} = new URL(req.url)
+
+    const page = searchParams.get("page")
+
+    const POST_PER_PAGE =2;
+    try {
+
+        const categories = await prisma.post.findMany({
+            take: POST_PER_PAGE, 
+            skip: POST_PER_PAGE * (page -1)
+        });
+        return new NextResponse(
+            JSON.stringify(categories, { status: 500 })
+        );
+
+    } catch (err) {
+        console.log(err);
+        return new NextResponse(
+            JSON.stringify({ message: "Something went wrong!" }, { status: 500 })
+        );
+    }
+}
