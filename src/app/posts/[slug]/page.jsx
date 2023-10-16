ce09c9280c60a3bf30fd9fdc04db38fbdecc215e
shahmeer <theshahmeerfsd@gmail.com>
2023-10-16 17:32:19 +0500

    Impleme.. API to fetch single post with user info

--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -4,34 +4,51 @@ import styles from './singlePage.module.css'
 import Image from 'next/image'
 import React from 'react'
 
-const SinglePage = () => {
+const getData = async (slug) => {
+    const res = await fetch(`http://localhost:3000/api/posts/${slug}`, {
+      cache: "no-store",
+    });
+    
+    if (!res.ok) {
+      throw new Error(`Failed to fetch data. Status`);
+    }
+  
+    return res.json()
+  
+  }
+
+const SinglePage = async ({params}) => {
+
+    const { slug } = params;
+
+    const data = await getData(slug)
   return (
     <div className={styles.container}>
         <div className={styles.infoContainer}>
             <div className={styles.textContainer}>
-                <h1 className={styles.title}>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h1>
+                <h1 className={styles.title}>
+                    {data?.title}
+                </h1>
                 <div className={styles.user}>
+                    {data?.user?.image && (
                     <div className={styles.userImageContainer}>
-                        <Image src='/p1.jpeg' alt='' fill className={styles.avatar} />
+                        <Image src={data?.user.image} alt='' fill className={styles.avatar} />
                     </div>
+                    )}
                     <div className={styles.userTextContainer}>
-                        <span className={styles.username}>John Doe</span>
+                        <span className={styles.username}>{data?.user.name }</span>
                         <span className={styles.date}>11.01.2023</span>
                     </div>
                 </div>
             </div>
+            {data?.img && (
             <div className={styles.imageContainer}>
                 <Image src='/p1.jpeg' alt='' fill className={styles.image} />
             </div>
+            )}
         </div>
         <div className={styles.content}>
         <div className={styles.post}>
-            <div className={styles.description}>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla voluptatibus obcaecati non provident suscipit quo repellendus, ipsa nobis ipsum illum quidem at ea modi. Tempore doloribus aperiam quibusdam accusamus molestias aliquam consectetur cupiditate eligendi, quos repellendus dolorem velit voluptates laborum, molestiae dolores veniam?</p>
-                <h2>Lorem ipsum dolor sit amet.</h2>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo praesentium et incidunt rem voluptatibus, totam fugiat nostrum maxime odit sit animi explicabo eos laudantium dolore perspiciatis. Earum dicta neque totam facere harum.</p>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo praesentium et incidunt rem voluptatibus, totam fugiat nostrum maxime odit sit animi explicabo eos laudantium dolore perspiciatis. Earum dicta neque totam facere harum.</p>
-            </div>
             <div className={styles.comment}>
             <Comments />
             </div>  
